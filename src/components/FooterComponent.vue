fba3c00b37341889253021bb679e150e5d6c84d5
ezTxmMC <eztxmmc@hotmail.com>
2025-05-05 22:48:14 +0200

    added footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="w-full h-64 flex justify-center items-center bg-[#1E1E1E] text-stone-300">
+    <div class="w-3/4 h-full grid items-center grid-flow-col auto-cols-auto gap-4">
+      <div class="w-100 h-4/5 flex flex-col">
+        <p class="text-white text-lg font-semibold">2weeksmc Handrick & Kaemereit GbR</p>
+        <p class="mb-4 text-white">Copyright &copy; 2025</p>
+        <p>We are a hosting company for fast and powerful dockerized minecraft servers.</p>
+      </div>
+      <div class="w-50 h-4/5 flex flex-col gap-2">
+        <p class="mb-2 text-white text-lg font-semibold">Company</p>
+        <p>About</p>
+        <p>Team</p>
+        <p>Carrier</p>
+        <p>Blog</p>
+      </div>
+      <div class="w-50 h-4/5 flex flex-col gap-2">
+        <p class="mb-2 text-white text-lg font-semibold">Support</p>
+        <p>Contact</p>
+        <p>Ticketsystem</p>
+        <p>Status</p>
+        <p>FAQ</p>
+      </div>
+      <div class="w-50 h-4/5 flex flex-col gap-2">
+        <p class="mb-2 text-white text-lg font-semibold">Legal</p>
+        <p>Terms of Service</p>
+        <p>Privacy policy</p>
+        <p>Imprint</p>
+      </div>
+    </div>
+  </div>
+</template>
