fc5cf7d4a34878fbc0f49b99a57d99a14a36ba6f
ezTxmMC <eztxmmc@hotmail.com>
2025-05-06 11:59:07 +0200

    made card component top slower

--- src/components/home/CardComponent.vue
+++ src/components/home/CardComponent.vue
@@ -17,7 +17,7 @@ defineProps({
 
 <template>
   <div
-    class="p-4 w-64 h-60 flex flex-col items-center bg-[#1E1E1E] rounded-2xl cursor-pointer duration-200 ease-in-out hover:-translate-y-2"
+    class="p-4 w-64 h-60 flex flex-col items-center bg-[#1E1E1E] rounded-2xl cursor-pointer duration-300 ease-in-out hover:-translate-y-2"
   >
     <div class="w-16 h-16 flex justify-center items-center bg-[#FF8C00] rounded-xl">
       <img :src="'/' + img + '.svg'" alt="" class="w-12 h-12" />
