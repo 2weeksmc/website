f27d2a29d5f245eac7316fb06e2383795edf4639
ezTxmMC <eztxmmc@hotmail.com>
2025-05-08 12:19:45 +0200

    removed slider component and using input range temporary

--- src/views/main/PricingView.vue
+++ src/views/main/PricingView.vue
@@ -2,12 +2,27 @@
 import { ref } from 'vue'
 import BannerComponent from '@/components/BannerComponent.vue'
 import PricingCardComponent from '@/components/pricing/PricingCardComponent.vue'
-import SliderComponent from '@/components/pricing/SliderComponent.vue'
+import { onMounted } from 'vue'
+
+const isOnCooldown = ref(false)
 
 const ram = ref(2)
 const storage = ref(16)
-const plugins = ref(5)
-const mods = ref(5)
+const plugins = ref(0)
+const mods = ref(0)
+const price = ref(0)
+
+function calculatePrice() {
+  const ramPrice = ram.value * 0.5
+  const storagePrice = storage.value * 0.1
+  const pluginsPrice = plugins.value * 0.025
+  const modsPrice = mods.value * 0.05
+  price.value = ramPrice + storagePrice + pluginsPrice + modsPrice
+}
+
+onMounted(() => {
+  calculatePrice()
+})
 </script>
 
 <template>
@@ -21,33 +36,33 @@ const mods = ref(5)
             color="#aaaaaa"
             name="Starter"
             cpu="AMD Ryzen 5950X"
-            ram="2048"
+            ram="2"
             storage="16"
             plugin-support-amount="5"
             mod-support-amount="0"
-            price="0.00"
+            :price="isOnCooldown ? '1.99' : '0.00'"
             special="Only runs for 2 weeks. Available next after 3 months."
           />
           <PricingCardComponent
             color="#45f84f"
             name="Advanced"
             cpu="AMD Ryzen 5950X"
-            ram="4096"
+            ram="4"
             storage="32"
             plugin-support-amount="10"
             mod-support-amount="10"
-            price="4.99"
+            price="3.99"
             popular="true"
           />
           <PricingCardComponent
             color="#ffe942"
             name="Professional"
             cpu="AMD Ryzen 5950X"
-            ram="6144"
+            ram="6"
             storage="48"
             plugin-support-amount="25"
-            mod-support-amount="50"
-            price="7.99"
+            mod-support-amount="25"
+            price="6.99"
           />
           <div
             :class="{
@@ -76,15 +91,73 @@ const mods = ref(5)
               type="button"
               value="Configurator"
               class="w-2/3 h-10 bg-[#FF8C00] rounded-lg text-xl"
+              onclick="document.getElementById('configurator').scrollIntoView()"
             />
           </div>
         </div>
-        <p class="w-full text-center text-2xl font-semibold">Configurator</p>
-        <div class="mt-2 mb-4 px-8 w-full">
-          <SliderComponent label="RAM" :min="2" :max="16" :step="2" v-model="ram" />
-          <SliderComponent label="NVMe Storage" :min="16" :max="128" :step="16" v-model="storage" />
-          <SliderComponent label="Plugins" :min="0" :max="50" :step="5" v-model="plugins" />
-          <SliderComponent label="Mods" :min="0" :max="100" :step="5" v-model="mods" />
+        <p id="configurator" class="w-full text-center text-2xl font-semibold">Configurator</p>
+        <div class="w-full">
+          <label for="ram" class="ml-32 mr-24 flex flex-col">
+            <div class="flex">
+              RAM: <a>{{ ram }} GB</a>
+            </div>
+            <input
+              id="ram"
+              type="range"
+              min="2"
+              max="16"
+              step="2"
+              value="2"
+              v-model="ram"
+              @input="calculatePrice"
+            />
+          </label>
+          <label for="nvme" class="ml-32 mr-24 flex flex-col">
+            <div class="flex">
+              NVMe: <a>{{ storage }} GB</a>
+            </div>
+            <input
+              id="nvme"
+              type="range"
+              min="16"
+              max="128"
+              step="16"
+              value="16"
+              v-model="storage"
+              @input="calculatePrice"
+            />
+          </label>
+          <label for="plugins" class="ml-32 mr-24 flex flex-col">
+            <div class="flex">
+              Plugins: <a>{{ plugins }}</a>
+            </div>
+            <input
+              id="plugins"
+              type="range"
+              min="0"
+              max="50"
+              step="5"
+              value="0"
+              v-model="plugins"
+              @input="calculatePrice"
+            />
+          </label>
+          <label for="mods" class="ml-32 mr-24 flex flex-col">
+            <div class="flex">
+              Mods: <a>{{ mods }}</a>
+            </div>
+            <input
+              id="mods"
+              type="range"
+              min="0"
+              max="100"
+              step="5"
+              value="0"
+              v-model="mods"
+              @input="calculatePrice"
+            />
+          </label>
+          <p class="w-full text-center">{{ price.toFixed(2) }}</p>
         </div>
       </div>
     </div>
